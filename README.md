1bea0af04e1b6792c548ee04021c477fc3ca80b1
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-08-27 17:04:48 +0200

    V0.1 - Game setup nearly complete

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Proj-Rock-Paper-Scissors
+
+**Projekt lev3_9: Rock, Paper, Scissors**
+
+Aufgabenstellung:
+
+Jeder hat in seinem Leben schonmal "Rock, Paper, Scissors" (Schnick, Schnack, Schnuck) gespielt. Ihr kennt die Regeln also bereits. ;)
+
+Definiere, wie viele Runden wir gegen den Computer spielen werden.<br>
+Die Farbe sollte sich ändern, wenn es sich um einen Gewinn, ein Unentschieden oder eine Niederlage handelt. Das Design kannst du natürlich auch anders gestalten.
+
+---
+
+_Source:_ https://github.com/Ninni-cfm/Proj-Rock-Paper-Scissors
+
+_Demo:_ https://ninni-cfm.github.io/Proj-Rock-Paper-Scissors/
---
+++ assets/js/main.js
@@ -0,0 +1,241 @@
+// constant definitions for UI container elements
+const gameSetup = document.getElementById('gameSetup');
+const gamePlay = document.getElementById('gamePlay');
+
+// constant definitions for single UI elements
+const numCustomRounds = document.getElementById('numCustomRounds');
+
+// constant definitions for the moves
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+
+// constant definitions for visibility (improves readability of code)
+const hidden = false;
+const visible = true;
+
+// variable definitions for the game
+let maxRounds = 5;
+let playedRounds = 0;
+let playerPoints = 0;
+let computerPoints = 0;
+
+// an array to store all player moves to detect some 'possible preferences'
+// init with 1 for every move
+let playerMoves = [1, 1, 1];
+
+
+
+initialize();
+function initialize() {
+
+    document.getElementById('rbRounds5').checked = true;
+
+    // first setup rounds ...
+    showHideElement(gameSetup, visible);
+    showHideElement(gamePlay, hidden);
+}
+
+
+
+//****************************************************************************
+// The party begins...
+function startGame() {
+
+    maxRounds = getNumberOfRounds();
+    playedRounds = 0;
+    playerPoints = 0;
+    computerPoints = 0;
+
+    showHideElement(gameSetup, hidden);
+    showHideElement(gamePlay, visible);
+}
+
+
+
+//****************************************************************************
+// get number of rounds
+function getNumberOfRounds() {
+
+    for (let i = 5; i <= 20; i += 5) {
+        maxRounds = document.getElementById(`rbRounds${i}`).checked ? i : maxRounds;
+    }
+
+    if (document.getElementById('rbCustom').checked) {
+        maxRounds = numCustomRounds.value;
+    }
+
+    return maxRounds;
+}
+
+//****************************************************************************
+function makeMove(playerMove) {
+
+    let result = 0;
+
+    playerMove = eval(playerMove);
+
+    let computerMove = getComputerMove();
+
+    switch (true) {
+        case (playerMove == computerMove):
+            // draw (default): result = 0;
+            break;
+
+        case (playerMove == rock):
+            result = computerMove == scissors ? 1 : -1;
+            break;
+
+        case (playerMove == paper):
+            result = computerMove == rock ? 1 : -1;
+            break;
+
+        case (playerMove == scissors):
+            result = computerMove == paper ? 1 : -1;
+            break;
+
+        default:
+            // illegal move, should never happen! But who knows...
+            console.log("%cWTF! I'm Lost!", "color: red;");
+            console.log("playerMove:", playerMove)
+            return;
+    }
+
+    // add result to points
+    if (result != 0) result > 0 ? playerPoints++ : computerPoints++;
+
+    // remember player's move
+    playerMoves[playerMove]++;
+
+
+    // increase the number of played rounds    
+    playedRounds++;
+
+    // show the result of this round
+    showResult(result);
+}
+
+//****************************************************************************
+function getComputerMove() {
+
+    let computerMove;
+
+    // a factor to make the result more 'accurate'
+    let factor = 1000;
+
+    // try to detect some 'possible preferences'
+    let sum = factor * (
+        playerMoves[rock] +
+        playerMoves[paper] +
+        playerMoves[scissors]
+    );
+
+    // get a random number 
+    let response = Math.random() * sum;
+
+    // and now check the player's preferences
+    if (response < playerMoves[rock] * factor) {
+
+        // player prefers rock, but paper wraps stone!
+        computerMove = paper;
+
+    } else if (response < playerMoves[paper] * factor) {
+
+        // player prefers paper, but scissors cuts paper!
+        computerMove = scissors;
+
+    } else {
+
+        // player prefers scissors, but stone rock scissors!
+        computerMove = rock;
+    }
+
+    // return computer's move
+    return computerMove;
+}
+
+
+
+//****************************************************************************
+function showResult(result) {
+    // a simple output:
+    switch (result) {
+        case -1: console.log("Computer wins!"); break;
+        case 0: console.log("Draw!"); break;
+        case 1: console.log("Player wins!"); break;
+    }
+
+    console.log(`Round ${playedRounds}: Player - ${playerPoints} : ${computerPoints} - Computer`);
+
+    // game hasn't ended...
+    if (playedRounds < maxRounds) return;
+
+    let text = 'Draw';
+    // TODO: show final game result
+    if (playerPoints != computerPoints) text = playerPoints > computerPoints ? 'Player wins!' : 'Computer wins!';
+
+    alert(`Game ended, ${text}`)
+
+    showEndGame();
+
+
+}
+
+
+//****************************************************************************
+function showEndGame() {
+
+
+    // finally show the setup screen again
+    showHideElement(gameSetup, visible);
+    showHideElement(gamePlay, hidden);
+}
+
+//****************************************************************************
+function resetGame() {
+
+}
+
+
+//****************************************************************************
+function getElement(id) {
+    return document.getElementById(id);
+}
+
+function showHideElement(element, isVisible) {
+
+    // element is visible, nothing to do...
+    if (isVisible && (element.style.display != "none"))
+        return;
+
+    // element is visible and should be hidden
+    if (!isVisible && (element.style.display != "none")) {
+        element["save"] = element.style.display;
+        element.style.display = "none";
+    }
+
+    // element is hidden and should be visible
+    if (isVisible && (element.style.display = "none")) {
+        element.style.display = element["save"];
+        element["save"] = null;
+    }
+}
+
+
+
+
+
+
+
+
+
+//****************************************************************************
+// speak(document.getElementById("txtSpeak").value);
+function speak(textToSpeak) {
+
+    let utterance = new SpeechSynthesisUtterance(textToSpeak);
+    speechSynthesis.speak(utterance);
+}
+
+
+
